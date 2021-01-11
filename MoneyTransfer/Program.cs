0d998e951e8bf2caf74e6d0bbfa244a7e1bd37fd
ooepi <robert.harmaakivi@gmail.com>
2021-01-12 00:15:24 +0200

    First part complete, money transferrable between Joe nad Bob

--- MoneyTransfer/Program.cs
+++ MoneyTransfer/Program.cs
@@ -10,6 +10,42 @@ namespace MoneyTransfer
     {
         static void Main(string[] args)
         {
+            Guy joe = new Guy() { Name = "Joe", Cash = 50 };
+            Guy bob = new Guy() { Cash = 100, Name = "Bob" };
+
+            while (true)
+            {
+                joe.WriteMyInfo();
+                bob.WriteMyInfo();
+
+                Console.WriteLine("Enter an amount: ");
+                string howMuch = Console.ReadLine();
+                if (howMuch == "") return;
+
+                if (int.TryParse(howMuch, out int amount))
+                {
+                    Console.Write("Who should give the cash: ");
+                    string whichGuy = Console.ReadLine();
+                    if (whichGuy == "Joe")
+                    {
+                        joe.GiveCash(amount);
+                        bob.ReceiveCash(amount);
+                    }
+                    else if (whichGuy == "Bob")
+                    {
+                        bob.GiveCash(amount);
+                        joe.ReceiveCash(amount);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please write 'Joe' or 'Bob'");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please enter an amount (Or a blank line to exit.)");
+                }
+            }
         }
     }
 }
